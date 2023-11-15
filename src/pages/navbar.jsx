3633b8a7a873a5e581f87d23e35511dd4c5ccf5d
gameloker <86136349+gameloker@users.noreply.github.com>
2023-11-15 14:08:26 -0300

    actualizacion

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="p-4">
+    <Navbar bg="dark" variant="dark" expand="lg" className="p-4" >
       <Container>
         <Navbar.Brand href="#home">Mi Portafolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- src/pages/sobremi.jsx
+++ src/pages/sobremi.jsx
@@ -3,12 +3,20 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const SobreMi = () => {
   return (
-    <Container id="about" style={{paddingTop:'150px', paddingBottom:'150px'}}>
+    <Container
+      id="about"
+      style={{
+        paddingTop: '150px',
+        paddingBottom: '150px',
+        position: 'relative',
+        zIndex: '9999', // Ensure that the z-index is higher than the particle background
+      }}
+    >
       <Row>
         <Col>
           <h2>Acerca de Mí</h2>
           <p>¡Hola! Soy Juan Pablo Ruz, un desarrollador web fullStack apasionado...</p>
-          {/* Añade más detalles sobre ti */}
+          {/* Add more details about yourself */}
         </Col>
       </Row>
     </Container>
