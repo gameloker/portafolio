0900817bfdc8ffe9e0b0dd07801e020f894f931d
gameloker <86136349+gameloker@users.noreply.github.com>
2023-12-21 07:51:13 -0300

    ac

--- src/pages/sobremi.jsx
+++ src/pages/sobremi.jsx
@@ -64,7 +64,7 @@ const SobreMi = () => {
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4   lg:order-3 lg:text-right lg:self-center   ">
-                    <div className="py-6 px-3 text-center  lg:mt-0 md:mt-32 sm:mt-32 mt-36  ">
+                    <div className="py-6 px-3 text-center  lg:mt-0 md:mt-32 sm:mt-32 mt-36 xl:mt-0  ">
                     <button
         className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none  mb-1 ease-linear transition-all duration-150 sm:items-center sm:justify-center "
         type="button"
