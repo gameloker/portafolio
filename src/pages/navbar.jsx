d512c63e4f831d4f10416e99798fe9432976e6ca
gameloker <86136349+gameloker@users.noreply.github.com>
2023-12-21 06:42:45 -0300

    actualizacion

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,31 +1,36 @@
-import React,{useEffect} from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
+const MyNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
 
-
-const Header = () => {
-  const location = useLocation();
-  console.log(location.pathname);
-  
-  
-    
-
+  const closeNavbar = () => setExpanded(false);
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="p-4 bg-black" style={{zIndex:'2'}} >
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      className="p-4 bg-black"
+      style={{ zIndex: '2' }}
+      expanded={expanded}
+    >
       <Container>
         <Navbar.Brand href="#home">Mi Portafolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav " id="buttonNav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/" onClick={closeNavbar}>
               Sobre Mí
             </Nav.Link>
-            <Nav.Link as={Link} to="/proyectos">
+            <Nav.Link as={Link} to="/proyectos" onClick={closeNavbar}>
               Proyectos
             </Nav.Link>
-            <Nav.Link as={Link} to="/contacto">
+            <Nav.Link as={Link} to="/contacto" onClick={closeNavbar}>
               Contacto
             </Nav.Link>
           </Nav>
@@ -35,4 +40,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default MyNavbar;
--- src/pages/sobremi.jsx
+++ src/pages/sobremi.jsx
@@ -64,13 +64,13 @@ const SobreMi = () => {
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
+                    <div className="py-6 px-3 mt-32 sm:mt-32 lg:mt-0 sm:p-32  text-center">
                     <button
-        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none  mb-1 ease-linear transition-all duration-150 sm:items-center sm:justify-center "
         type="button"
         onClick={handleDownload}
       >
-        Descargar Currículum
+        Descargar CV
       </button>
                     </div>
                   </div>
